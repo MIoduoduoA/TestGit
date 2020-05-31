10faae5d3429d1a19a294d8cd6009048da1abb62
tom_glb <goodMorning_glb@atguigu.com>
2020-05-31 11:43:30 +0800

    test TestGitA left

--- TestGit/src/main/java/com/bjsxt/git/FirstTest.java
+++ TestGit/src/main/java/com/bjsxt/git/FirstTest.java
@@ -3,5 +3,6 @@ package com.bjsxt.git;
 public class FirstTest {
 	public static void main(String[] args) {
 		System.out.println("FirstTest.main()");
+		System.out.println("test TestGitA left");
 	}
 }
